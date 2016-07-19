1305a664e5c84ec88d6caba4b81a95f7c1c5f5d5
Michael Fortin <michael@m410.us>
2016-07-19 11:16:40 -0400

    Partially testing loading

--- src/main/java/org/m410/config/YamlConfiguration.java
+++ src/main/java/org/m410/config/YamlConfiguration.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 import java.io.Writer;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
@@ -35,9 +36,25 @@ public class YamlConfiguration extends BaseHierarchicalConfiguration implements 
 
     public void read(Reader in) throws ConfigurationException, IOException {
         ImmutableNode.Builder rootBuilder = new ImmutableNode.Builder();
-        ImmutableNode top = rootBuilder.create();
-        Map<ImmutableNode, Object> elemRefMap = toNodeMap(yaml.load(in), top);
+        ImmutableNode root = rootBuilder.create();
+        Map<ImmutableNode, ?> elemRefMap = toNodeMap(yaml.load(in), root);
+        ImmutableNode top = addChildrenToRoot(root, elemRefMap);
         getSubConfigurationParentModel().mergeRoot(top, "yaml", elemRefMap, null, this);
+    }
+
+    private ImmutableNode addChildrenToRoot(ImmutableNode root, Map<ImmutableNode, ?> elemRefMap) {
+        ImmutableNode top = root;
+
+        for (ImmutableNode immutableNode : elemRefMap.keySet()) {
+            if(elemRefMap.get(immutableNode) instanceof Map) {
+                top = top.addChild(addChildrenToRoot(immutableNode, (Map)elemRefMap.get(immutableNode)));
+            }
+            else {
+                top = top.addChild(immutableNode);
+            }
+        }
+
+        return top;
     }
 
     public void write(Writer out) throws ConfigurationException, IOException {
@@ -45,22 +62,21 @@ public class YamlConfiguration extends BaseHierarchicalConfiguration implements 
     }
 
     private Map<ImmutableNode, Object> toNodeMap(Object load, ImmutableNode parent) {
-        final Map<String,Object> map = (Map<String,Object>)load;
+        final Map<String,?> map = (Map<String,?>)load;
         final Map<ImmutableNode, Object> nodeMap = new HashMap<>();
 
         for (String key : map.keySet()) {
             Object value = map.get(key);
-            ImmutableNode immutableNode = new ImmutableNode.Builder().name(key).create();
-            parent.addChild(immutableNode);
+            ImmutableNode currentNode = new ImmutableNode.Builder().name(key).value(value).create();
 
             if(value instanceof String || value instanceof Number || value instanceof Boolean) {
-                nodeMap.put(immutableNode, value);
+                nodeMap.put(currentNode, value);
             }
             else if(value instanceof Map) {
-                toNodeMap(value, immutableNode);
+                nodeMap.put(currentNode, toNodeMap(value, currentNode));
             }
             else if (value instanceof Collection){
-                nodeMap.put(immutableNode, value);
+                nodeMap.put(currentNode, value);
             }
         }
 
--- src/test/java/org/m410/config/YamlConfigurationTest.java
+++ src/test/java/org/m410/config/YamlConfigurationTest.java
@@ -2,10 +2,12 @@ package org.m410.config;
 
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.ConversionHandler;
+import org.apache.commons.configuration2.interpol.ConfigurationInterpolator;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Iterator;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -22,23 +24,50 @@ public class YamlConfigurationTest {
     @Test
     public void read() throws Exception {
         Parameters params = new Parameters();
-        FileBasedConfigurationBuilder<YamlConfiguration> builder = new FileBasedConfigurationBuilder<>(YamlConfiguration.class)
+        FileBasedConfigurationBuilder<YamlConfiguration> builder =
+                new FileBasedConfigurationBuilder<>(YamlConfiguration.class)
                         .configure(params.xml().setFileName("src/test/resource/test1.yml"));
+
         YamlConfiguration config = builder.getConfiguration();
         assertNotNull(config);
+
         System.out.println(config.getRootElementName());
         final Iterator<String> keys = config.getKeys();
 
         while (keys.hasNext()) {
-            System.out.println(config.getKeys().next());
+            System.out.println(keys.next());
         }
 
-        assertEquals("test",config.getString("yaml.test"));
+        try{
+            config.getBoolean("test-empty");
+            fail();
+        }
+        catch (NoSuchElementException x) {
+            assertNotNull(x);
+        }
+
+        assertNull(config.getString("test-empty"));
+        assertEquals("test value",config.getString("test-string"));
+        assertEquals("test value",config.getString("test-place"));
+        assertEquals("test value",config.getString("test-interp"));
+        assertEquals("/Library/Java/JavaVirtualMachines/jdk1.8.0_92.jdk/Contents/Home",config.getString("test-env"));
+        assertEquals(12,config.getInt("test-int"));
+        assertEquals(Integer.valueOf(12),config.getInteger("test-int",0));
+        assertEquals(true,config.getBoolean("test-boolean"));
+        assertNotNull(config.getString("test-nested.one"));
+
+        assertEquals("test spaces",config.getString("test string with spaces"));
+
+        Collection<String> collection = config.getCollection(String.class, "test-collection", null);
+        assertEquals(3, collection.size());
+        assertEquals("one",collection.iterator().next());
+        assertEquals("one",config.getList("test-collection").get(0));
     }
 
     @Test
     public void write() throws Exception {
 
     }
+
 
 }
