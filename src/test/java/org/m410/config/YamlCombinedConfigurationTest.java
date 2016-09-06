f168b0f95f804d849f24ad9dade2aba2b5e86ea5
Michael Fortin <michael@m410.us>
2016-09-06 09:42:48 -0400

    reformat

--- src/test/java/org/m410/config/YamlCombinedConfigurationTest.java
+++ src/test/java/org/m410/config/YamlCombinedConfigurationTest.java
@@ -4,14 +4,11 @@ import org.apache.commons.configuration2.CombinedConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.apache.commons.configuration2.tree.MergeCombiner;
 import org.apache.commons.configuration2.tree.UnionCombiner;
 import org.junit.Test;
 
-
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -48,41 +45,41 @@ public class YamlCombinedConfigurationTest {
         System.out.println("---");
         System.out.println(output);
         System.out.println("---");
-        assertEquals(3, (output.length() - output.replace("- ", "").length())/2);
+        assertEquals(3, (output.length() - output.replace("- ", "").length()) / 2);
     }
 
     @Test
     public void testComposite() throws ConfigurationException {
 
         final YamlConfiguration defaultConfig = new FileBasedConfigurationBuilder<>(YamlConfiguration.class)
-                        .configure(new Parameters().hierarchical().setFileName("src/test/resources/test1.yml"))
-                        .getConfiguration();
+                .configure(new Parameters().hierarchical().setFileName("src/test/resources/test1.yml"))
+                .getConfiguration();
 
         final YamlConfiguration overlapConfig = new FileBasedConfigurationBuilder<>(YamlConfiguration.class)
-                        .configure(new Parameters().hierarchical().setFileName("src/test/resources/test2.yml"))
-                        .getConfiguration();
+                .configure(new Parameters().hierarchical().setFileName("src/test/resources/test2.yml"))
+                .getConfiguration();
 
         CombinedConfiguration combined = new CombinedConfiguration(new UnionCombiner());
-        combined.addConfiguration(overlapConfig,"app");
-        combined.addConfiguration(defaultConfig,"default");
+        combined.addConfiguration(overlapConfig, "app");
+        combined.addConfiguration(defaultConfig, "default");
         YamlConfiguration configuration = new YamlConfiguration(combined);
 
-        assertEquals(1222,configuration.getInt("int"));
-        assertEquals(false,configuration.getBoolean("boolean"));
-        assertEquals("other",configuration.getString("module(org..m410..persistence:jpa).user"));
-        assertEquals("some-password",configuration.getString("module(org..m410..persistence:jpa).password"));
+        assertEquals(1222, configuration.getInt("int"));
+        assertEquals(false, configuration.getBoolean("boolean"));
+        assertEquals("other", configuration.getString("module(org..m410..persistence:jpa).user"));
+        assertEquals("some-password", configuration.getString("module(org..m410..persistence:jpa).password"));
 
-        assertEquals("one",configuration.getString("collection-of-map(0).key"));
-        assertEquals("two",configuration.getString("collection-of-map(1).key"));
-        assertEquals("three",configuration.getString("collection-of-map(2).key"));
-        assertEquals("four",configuration.getString("collection-of-map(3).key"));
-        assertEquals("half",configuration.getString("collection-of-map(4).key"));
+        assertEquals("one", configuration.getString("collection-of-map(0).key"));
+        assertEquals("two", configuration.getString("collection-of-map(1).key"));
+        assertEquals("three", configuration.getString("collection-of-map(2).key"));
+        assertEquals("four", configuration.getString("collection-of-map(3).key"));
+        assertEquals("half", configuration.getString("collection-of-map(4).key"));
 
         assertEquals("four on test2", configuration.getString("collection(0)"));
-        assertEquals("three",configuration.getString("nested.one"));
-        assertEquals("four",configuration.getString("nested.two"));
-        assertEquals("five",configuration.getString("nested.three"));
-        assertEquals("sub-three",configuration.getString("nested.four.four-sub1"));
+        assertEquals("three", configuration.getString("nested.one"));
+        assertEquals("four", configuration.getString("nested.two"));
+        assertEquals("five", configuration.getString("nested.three"));
+        assertEquals("sub-three", configuration.getString("nested.four.four-sub1"));
 
     }
 }
